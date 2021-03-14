51af7c09a34a7d7f00366c53f95fa17ff0ba00bf
Ond Re <aapetrulevich@icloud.com>
2021-03-14 21:02:49 +0300

    Refactor echo server

--- src/ru/geekbrains/chat/ClientHandler.java
+++ src/ru/geekbrains/chat/ClientHandler.java
@@ -21,24 +21,71 @@ public class ClientHandler {
             this.name = "";
 
             new Thread(() -> {
-                autentication();
-                readMessage();
-            });
+                try {
+                    authentication();
+                    readMessage();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    closeConnection();
+                }
+            }).start();
         } catch (IOException e) {
-            System.out.println(e.getMessage());;
-        } finally {
-            closeConnection();
+            System.out.println("Server problem");;
         }
     }
 
-    public void sendMessage(String message) {
+    private void authentication() throws IOException {
+        while (true) {
+            String authStr = dis.readUTF();
+            if (authStr.startsWith("/auth")) {
+                String [] arr = authStr.split("\\s");
+                String nick = echoServer
+                        .getAuthService()
+                        .getNickByLoginAndPassword(arr[1], arr[2]);
+                if (!nick.isEmpty()) {
+                    if (!echoServer.isNickBusy(nick)) {
+                        sendMessage("/authok " + nick);
+                        name = nick;
+                        echoServer.sendMessageToClients(nick + "Joined to chat");
+                        echoServer.subscribe(this);
+                        return;
+                    } else {
+                        sendMessage("This " + name + " is busy!");
+                    }
+                } else {
+                    sendMessage("Wrong login/password");
+                }
+            }
+        }
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void sendMessage(String message) {
+        try {
+            dos.writeUTF(message);
+        } catch (IOException ignored) {
+        }
     }
+
+    private void readMessage() throws IOException {
+        while (true) {
+            String messageFromClient = dis.readUTF();
+            if (messageFromClient.equals("/q")) {
+                sendMessage(messageFromClient);
+                break;
+            }
+            echoServer.sendMessageToClients(name + ": " + messageFromClient);
+
+        }
+    }
+
+    private void closeConnection() {
+        echoServer.subscribe(this);
+        echoServer.sendMessageToClients(name + " leave the chat.");
+    }
+
 }
--- src/ru/geekbrains/chat/EchoServer.java
+++ src/ru/geekbrains/chat/EchoServer.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 public class EchoServer extends Thread {
     private Socket socket;
-    private ServerSocket serverSocket;
     private DataInputStream in;
     private DataOutputStream out;
     private boolean shouldShutdown;
@@ -31,10 +30,10 @@ public class EchoServer extends Thread {
             while (true) {
                 socket = serverSocket.accept();
                 new ClientHandler(this, socket);
+                clientsList = new ArrayList<>();
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
-            clientsList = new ArrayList<>();
         } finally {
             if (authService != null) {
                 authService.stop();
@@ -60,93 +59,7 @@ public class EchoServer extends Thread {
         return clientsList.stream().anyMatch(a -> a.getName().equals(nick));
     }
 
-    public void listen() {
-        System.out.println("Server is run, waiting for  connection...");
-        try {
-            socket = serverSocket.accept();
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("User connected");
-
-        Thread clientMessage = new Thread(this::clientMessagesRoutine);
-        Thread serverMessage = new Thread(this::serverMessageRoutine);
-
-        clientMessage.start();
-        serverMessage.start();
-
-        try {
-            clientMessage.join();
-            serverMessage.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void clientMessagesRoutine() {
-        while (true) {
-            String str = "";
-            try {
-                str = in.readUTF();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if (str.equals("/q")) {
-                try {
-                    System.out.println("Quit command received. Closing sockets");
-                    socket.close();
-                    serverSocket.close();
-                    synchronized(this) {
-                        shouldShutdown = true;
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                break;
-            } else {
-                writeOut("User: " + str);
-                System.out.println(str);
-            }
-        }
-    }
-
-    private void serverMessageRoutine() {
-        while(true) {
-            synchronized(this) {
-                if (shouldShutdown) {
-                    System.out.println("Terminating server message routine");
-                    break;
-                }
-            }
-            BufferedReader sm = new BufferedReader(new InputStreamReader(System.in));
-            String str;
-            try {
-                str = sm.readLine();
-                synchronized(this) {
-                    if (shouldShutdown) {
-                        System.out.println("Terminating server message routine");
-                        break;
-                    }
-                }
-                writeOut("Server: " + str);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private synchronized void writeOut(String text) {
-        try {
-            out.writeUTF(text);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
-        EchoServer server = new EchoServer();
-        server.listen();
+        new EchoServer();
     }
 }
